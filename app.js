8043eec98b79f5521e1fc0c773b936c2f148ed70
HereIsKam <HereIsKam@users.noreply.github.com>
2020-06-24 19:03:36 +0300

    Slider + Itunes API

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
+const slider = new Swiper ('.swiper-container', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
+
+search.addEventListener('submit', async function (ev) {
+    ev.preventDefault();
+    let value = this.term.value;
+    this.term.value = '';
+    let res = await fetch(`https://itunes.apple.com/search?term=${value}&entity=musicVideo&limit=3`)
+    let clips = await res.json()
+    console.log(clips);
+    clips.results.forEach(clip =>{
+        let slide = renderVideoSlide(clip)
+        slider.appendSlide(slide);
+    })
+})
 
 
 
-
-formtest.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    let data = {
-        text: this.term.value,
-        timestamp: Date.now()
-    };
-    console.log(data);
-    let res = await fetch('/test', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(data)
-    });
-    let answer = await res.text();
-    console.log(answer);
-    console.timeEnd('fetch')
-});
-
-
-
-
+function renderVideoSlide(object) {
+    return `<div class="swiper-slide"><video src="${object.previewUrl}" controls></video></div>`
+}
